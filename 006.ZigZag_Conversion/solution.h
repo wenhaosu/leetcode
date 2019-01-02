3a1ec5036c4e4a369b661643ba7da13cf615a28b
WenhaoSu <suwenhao@umich.edu>
2019-01-02 21:53:02 +0800

    Finish 006

---
+++ 006.ZigZag_Conversion/solution.h
@@ -0,0 +1,19 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        int keyNum = numRows * 2 - 2;
+        vector<string> retVec (numRows, "");
+        for (int i = 0; i < s.size(); i++){
+            if (i % keyNum < numRows) retVec[i % keyNum] += s[i];
+            else retVec[keyNum - i % keyNum] += s[i];
+        }
+        string ret;
+        for (const auto &str : retVec) ret += str;
+        return ret;
+    }
+};
---
+++ 006.ZigZag_Conversion/test.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "../Catch/catch.hpp"
+#include "solution.h"
+
+TEST_CASE("ZigZag Conversion", "[zigZagConversion]")
+{
+    Solution s;
+    REQUIRE( s.convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR");
+    REQUIRE( s.convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI");
+}
