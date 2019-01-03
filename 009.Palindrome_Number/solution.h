d296d85308f2c4bb24f225f1a12d0f023fe9b404
WenhaoSu <suwenhao@umich.edu>
2019-01-03 10:00:01 +0800

    Simplify code for 009

--- 009.Palindrome_Number/solution.h
+++ 009.Palindrome_Number/solution.h
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int temp = x;
-        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
-        unsigned long result = 0;
-        for( ; temp; temp = temp/10)
-            result = result*10 + temp%10;
+        if (x < 0) return false;
+        int temp = x, result = 0;
+        while (temp > 0) {
+            result = result * 10 + temp % 10;
+            temp /= 10;
+        }
         return (x == result);
     }
 };
