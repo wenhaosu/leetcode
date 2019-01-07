6bf6997465dbc07a5063a84e7b098e19ff82adbf
suwenhao <suwenhao@umich.edu>
2019-01-07 13:30:19 -0500

    Add another solution to 942

--- 942.DI_String_Match/solution.h
+++ 942.DI_String_Match/solution.h
@@ -5,16 +5,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> diStringMatch(string S) {
-        if (S.length() == 0) return {0};
-        vector<int> prev = diStringMatch(S.substr(1, S.length() - 1));
-        if (S[0] == 'I') {
-            for (auto &&item: prev) {
-                item++;
-            }
-            prev.insert(prev.begin(), 0);
-        } else {
-            prev.insert(prev.begin(), S.length());
+
+        /// Recursive Solution
+
+//        if (S.length() == 0) return {0};
+//        vector<int> prev = diStringMatch(S.substr(1, S.length() - 1));
+//        if (S[0] == 'I') {
+//            for (auto &item: prev) {
+//                item++;
+//            }
+//            prev.insert(prev.begin(), 0);
+//        } else {
+//            prev.insert(prev.begin(), S.length());
+//        }
+//        return prev;
+
+        /// Brute Force Solution
+
+        vector<int> res;
+        for (int l = 0, h = S.length(), i = 0; i <= S.length(); i++) {
+            res.push_back(S[i] == 'I' ? l++ : h--);
         }
-        return prev;
+        return res;
     }
 };
