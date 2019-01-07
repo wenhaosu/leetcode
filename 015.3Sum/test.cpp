351ab974d80e55dfb21de450ab01362bd0bfa689
suwenhao <suwenhao@umich.edu>
2019-01-07 12:58:49 -0500

    Fix bug in 015 tc

--- 015.3Sum/test.cpp
+++ 015.3Sum/test.cpp
@@ -3,10 +3,9 @@
 #include "../Catch/catch.hpp"
 #include "solution.h"
 
-TEST_CASE("Container With Most Water", "[containerWithMostWater]") {
+TEST_CASE("3 Sum", "[3Sum]") {
     Solution s;
     vector<int> test = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> ans = {{-1, 0,  1},
-                               {-1, -1, 2}};
+    vector<vector<int>> ans = {{-1, -1, 2}, {-1, 0, 1}};
     REQUIRE(s.threeSum(test) == ans);
 }
