97ea78622acf4481070ae2d3aa347e29560b1780
WenhaoSu <suwenhao@umich.edu>
2019-05-15 22:29:22 +0800

    Remove redundant headers

--- 001.Two_Sum/solution.h
+++ 001.Two_Sum/solution.h
@@ -1,6 +1,3 @@
-#include <unordered_map>
-#include <vector>
-
 using namespace std;
 
 class Solution {
--- 021.Merge_Two_Sorted_Lists/solution.h
+++ 021.Merge_Two_Sorted_Lists/solution.h
@@ -1,12 +1,10 @@
-#include <vector>
-
 using namespace std;
 
 struct ListNode {
     int val;
     ListNode *next;
 
-    ListNode(int x) : val(x), next(nullptr) {}
+    explicit ListNode(int x) : val(x), next(nullptr) {}
 };
 
 class Solution {
