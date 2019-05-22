302cf8cb96fabc6bf17b7de571d0942c94da902a
WenhaoSu <suwenhao@umich.edu>
2019-05-22 13:16:39 +0800

    Minor bug fix in 036

--- 036.Valid_Sudoku/solution.h
+++ 036.Valid_Sudoku/solution.h
@@ -7,8 +7,8 @@ public:
         vector<bool> status(9, false);
         for (int i = 0; i < 9; i++) {
             if (board[row][i] == '.') continue;
-            if (status[board[row][i] - '0']) return false;
-            status[board[row][i] - '0'] = true;
+            if (status[board[row][i] - '1']) return false;
+            status[board[row][i] - '1'] = true;
         }
         return true;
     }
@@ -17,8 +17,8 @@ public:
         vector<bool> status(9, false);
         for (int i = 0; i < 9; i++) {
             if (board[i][col] == '.') continue;
-            if (status[board[i][col] - '0']) return false;
-            status[board[i][col] - '0'] = true;
+            if (status[board[i][col] - '1']) return false;
+            status[board[i][col] - '1'] = true;
         }
         return true;
     }
@@ -28,8 +28,8 @@ public:
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (board[row + i][col + j] == '.') continue;
-                if (status[board[row + i][col + j] - '0']) return false;
-                status[board[row + i][col + j] - '0'] = true;
+                if (status[board[row + i][col + j] - '1']) return false;
+                status[board[row + i][col + j] - '1'] = true;
             }
         }
         return true;
