6188b00e9ced283784ba9036200254c2623b5817
WenhaoSu <suwenhao@umich.edu>
2019-05-23 16:38:52 +0800

    Improve algorithm for 402

--- 402.Remove_K_Digits/solution.h
+++ 402.Remove_K_Digits/solution.h
@@ -2,23 +2,16 @@ using namespace std;
 
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
-        if (k >= num.length()) return "0";
-        if (k == 0) {
-            while (num[0] == '0') num.erase(0, 1);
-            return num.empty() ? "0" : num;
+    string removeKdigits(const string& num, int k) {
+        string result;
+        for (char c : num) {
+            while (result.length() && result.back() > c && k) {
+                result.pop_back();
+                k--;
+            }
+            if (result.length() || c != '0') result.push_back(c);
         }
-        char leading = num[0], next_big = ' ';
-        for (int i = 1; i < num.length(); i++) {
-            if (next_big == ' ') {
-                if (leading <= num[i])
-                    next_big = num[i];
-                else if (leading > num[i])
-                    return removeKdigits(num.erase(i - 1, 1), k - 1);
-            } else if (next_big <= num[i]) {
-                next_big = num[i];
-            } else return removeKdigits(num.erase(i - 1, 1), k - 1);
-        }
-        return removeKdigits(num.erase(num.length() - 1, 1), k - 1);
+        while (result.length() && k--) result.pop_back();
+        return result.empty() ? "0" : result;
     }
 };
--- 402.Remove_K_Digits/test.cpp
+++ 402.Remove_K_Digits/test.cpp
@@ -5,8 +5,8 @@
 
 TEST_CASE("Remove K Digits", "[removeKdigits]") {
     Solution s;
-//    REQUIRE(s.removeKdigits("1432219", 3) == "1219");
-//    REQUIRE(s.removeKdigits("10200", 1) == "200");
-//    REQUIRE(s.removeKdigits("10", 2) == "0");
+    REQUIRE(s.removeKdigits("1432219", 3) == "1219");
+    REQUIRE(s.removeKdigits("10200", 1) == "200");
+    REQUIRE(s.removeKdigits("10", 2) == "0");
     REQUIRE(s.removeKdigits("112", 1) == "11");
 }
