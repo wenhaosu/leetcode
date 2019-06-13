a119b7c76b81ca9f1f182d1ee91b619f909459d6
WenhaoSu <suwenhao@umich.edu>
2019-06-13 14:35:26 +0800

    Finish 1008

--- 1008.Construct_Binary_Search_Tree_from_Preorder_Traversal/solution.h
+++ 1008.Construct_Binary_Search_Tree_from_Preorder_Traversal/solution.h
@@ -12,7 +12,19 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *bstFromPreorder(vector<int> &preorder) {
+        return helper(preorder.begin(), preorder.end());
+    }
 
+    TreeNode *helper(vector<int>::iterator start, vector<int>::iterator end) {
+        if (start >= end) return nullptr;
 
+        TreeNode *node = new TreeNode(*start);
+        auto left_node = start + 1;
+        auto right_node = upper_bound(start, end, *start);
+
+        node->left = helper(left_node, right_node);
+        node->right = helper(right_node, end);
+
+        return node;
     }
 };
