790ad213c09e9b0e2838d2bfbe8a988f0b3dce79
WenhaoSu <suwenhao@umich.edu>
2019-07-17 23:57:52 +0800

    Add another solution to 389

--- 389.Find_the_Difference/solution.h
+++ 389.Find_the_Difference/solution.h
@@ -3,16 +3,27 @@ using namespace std;
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> map;
-        for (char char_s : s) {
-            map[char_s]++;
-        }
-        for (char char_t : t) {
-            map[char_t]--;
-        }
-        for (auto item : map) {
-            if (item.second != 0) return item.first;
-        }
-        return 0;
+
+        /// Use hash map
+
+//        unordered_map<char, int> map;
+//        for (char char_s : s) {
+//            map[char_s]++;
+//        }
+//        for (char char_t : t) {
+//            map[char_t]--;
+//        }
+//        for (auto item : map) {
+//            if (item.second != 0) return item.first;
+//        }
+//        return 0;
+
+
+        /// User XOR
+
+        char r = 0;
+        for (char c:s) r ^= c;
+        for (char c:t) r ^= c;
+        return r;
     }
 };
